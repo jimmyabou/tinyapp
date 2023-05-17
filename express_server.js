90f2e6af73a3f8beb8ebee97b4a3c4e38793a8bc
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-16 20:50:21 -0400

    implemented the logout button to delete the cookie on logout

--- express_server.js
+++ express_server.js
@@ -33,69 +33,68 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
+});
+
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
- app.get("/urls", (req, res) => {
-  let user=req.cookies["username"];
-  const templateVars = { urls: urlDatabase ,username: user};
+});
+app.get("/urls", (req, res) => {
+  let user = req.cookies["username"];
+  const templateVars = { urls: urlDatabase, username: user };
   res.render("urls_index", templateVars);
   console.log(templateVars);
   console.log(user);
-  // res.render("urls_show", templateVars);
-  // res.render("urls_new", templateVars);
-  //res.render("./_header", templateVars);
-
 });
 
 
 app.get("/urls/new", (req, res) => {
-  let user=req.cookies["username"];
-  const templateVars = { urls: urlDatabase ,username: user};
+  let user = req.cookies["username"];
+  const templateVars = { urls: urlDatabase, username: user };
   res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  let shortId=generateRandomString();
-  urlDatabase[shortId]=req.body["longURL"];
-  let result=urlDatabase;
+  let shortId = generateRandomString();
+  urlDatabase[shortId] = req.body["longURL"];
+  let result = urlDatabase;
   res.redirect(`urls/${shortId}`);
   res.send(result); // Respond with 'Ok' (we will replace this)
 });
 app.get("/u/:id", (req, res) => {
-  let user=req.cookies["username"];
-  const templateVars = { urls: urlDatabase ,username: user};
+  let user = req.cookies["username"];
+  const templateVars = { urls: urlDatabase, username: user };
   res.render("urls_new", templateVars);
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
 app.get("/urls/:id", (req, res) => {
-  let user=req.cookies["username"];
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id],username: user };
+  let user = req.cookies["username"];
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: user };
   res.render("urls_show", templateVars);
 });
-app.post("/urls/:id/delete", (req,res)=>{
-  let deleteId=req.params.id;
+app.post("/urls/:id/delete", (req, res) => {
+  let deleteId = req.params.id;
   delete urlDatabase[deleteId];
   console.log(urlDatabase);
   res.redirect('/urls');
 })
-app.post("/urls/:id", (req,res)=>{
-  let EditId=req.params.id;
-  urlDatabase[EditId]=req.body.longURL;
+app.post("/urls/:id", (req, res) => {
+  let EditId = req.params.id;
+  urlDatabase[EditId] = req.body.longURL;
   console.log(urlDatabase);
   res.redirect(`/urls/${EditId}`);
 })
-app.post("/login", (req,res)=>{
-  let username=req.body.username;
-  res.cookie("username",username);
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", username);
   res.redirect(`/urls`);
 })
-
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect(`/urls`);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
